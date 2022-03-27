364673771ad3018403700d5d3f7c5b5b1e78145a
Ruth Wata <ruthnwata@gmail.com>
2022-03-26 23:26:58 -0600

    new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,32 @@ toggleBtn.addEventListener("click", function () {
 })
 
 
+
+// menuBtn
+
+let menuLink = document.querySelector(".menu-navrbarLinks")
+let toggleMenu = document.querySelector(".toggleBtn-menu")
+
+toggleMenu.addEventListener("click", function () {
+  
+  // navbarLink.classList.toggle("hidden");
+  
+  
+  if (menuLink.classList.contains("hidden")){
+    menuLink.classList.toggle("hidden");
+        // document.querySelector("h1").style.marginTop = "-4rem";
+    
+    // console.log("hi")
+    
+    
+  }else if (!menuLink.classList.contains("hidden")){
+  menuLink.classList.toggle("hidden");
+    
+    // document.querySelector("h1").style.marginTop = "-30.8rem";
+    
+  }
+})
+
+
+
+
